ea161745295928c5bf7f6f7bd50fcd3aadd3d0dd
ArulGnani <version47@gmail.com>
2020-05-21 01:52:41 +0530

    added pwa

---
+++ public/serviceWorker.js
@@ -0,0 +1,45 @@
+
+const CACHE_NAME = "spotly-v1"
+
+const cacheUrl = ["/","/personal"]
+
+// install 
+self.addEventListener("install",event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME) 
+            .then(cache => {
+                console.log("opened cache")
+                return cache.addAll(cacheUrl)
+            })
+    )
+})
+
+// handel fetch request 
+self.addEventListener('fetch',event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response
+                }else {
+                    return fetch(event.request)
+                }
+            })
+    )
+})
+
+// install and update
+self.addEventListener('activate',event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log("delted old cache")
+                        return caches.delete(cacheName)
+                    }
+                })
+            )
+        })
+    )
+})
